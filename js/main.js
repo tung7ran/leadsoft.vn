5df601b92ee1e39e8afaee8d468d3cf3c389ed6c
MinhHieu20 <72988848+MinhHieu20@users.noreply.github.com>
2022-09-15 17:12:53 +0700

    repair file scss,css,js

---
+++ js/main.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function(){
+    $('.home-store-list a').click(function() {
+
+      $('.home-store-list .home-store-list__item ').removeClass('active');
+      $(this).parent().addClass('active');
+  
+      // Display active tab
+      let currentTab = $(this).attr('href');
+      $('.tabs-content div').hide();
+      $(currentTab).show();
+  
+      return false;
+    });
+
+    $('.home-projects-top').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        nextArrow: false,
+        prevArrow: false,
+    });
+
+    $('.home-projects-between').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      dots: false,
+      centerMode: true,
+      focusOnSelect: true,
+      autoplay: true,
+      autoplaySpeed: 3500,
+      nextArrow: false,
+      prevArrow: false,
+  });
+
+  $('.home-projects-bottom').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    nextArrow: false,
+    prevArrow: false,
+});
+  
+});
+  
